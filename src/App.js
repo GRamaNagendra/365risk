800415a844ed73ab4a0aff748a42f65bf0ba2f1a
Rama Nagendra <ramanagendra2@gmail.com>
2024-06-26 02:41:27 +0530

    okey

--- src/App.js
+++ src/App.js
@@ -1,41 +1,10 @@
-import React from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
-import Layout from './components/Layout';
-import Home from './components/Home';
-import IndustryDetails from './components/IndustryDetails';
-import UIndustryDetails from './components/UIndustryDetails';
-import AddRisk from './components/AddRisk';
-import App2 from './JsonDataDisplay2';
-import Admin from './components/Admin';
-import RiskDetails from './components/Template';
-import Risks from './components/Risks.js';
-import Userinterface from './userinterface.js';
-import Notifications from './components/Notifications.js';
-import AboutUs from './AboutUs.js';
-import AddIndustry from './components/AddIndustry.js';
+function App(){
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="uindustries" element={<Userinterface />} />
-          <Route path="industries" element={<App2 />} />
-          <Route path="risks" element={<Risks />} />
-          <Route path="admin" element={<Admin />} />
-          <Route path="industry/:id" element={<IndustryDetails />} />
-          <Route path="uindustry/:id" element={<UIndustryDetails />} />
-          <Route path="industry/:id/risk/:riskId" element={<RiskDetails />} />
-          <Route path="industry/:id/addRisk" element={<AddRisk />} />
-          <Route path="add-risk" element={<AddRisk />} />
-          <Route path="addindustry" element={<AddIndustry />} />
-          <Route path="about" element={<AboutUs />} />
-          <Route path="notifications" element={<Notifications />} />
-        </Route>
-      </Routes>
-    </Router>
-  );
+
+  return(<div>
+
+    working
+  </div>);
 }
 
 export default App;
