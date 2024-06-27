f2a5235e9e52fb211cf75287f7bad68522a63b91
Rama Nagendra <ramanagendra2@gmail.com>
2024-06-27 12:30:58 +0530

    backendurl

--- src/api.js
+++ src/api.js
@@ -1,13 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "https://expert-umbrella-5wj44gvrx663vvpp-8080.app.github.dev/",
+    baseURL: "https://symmetrical-carnival-wwp99rv5vvq2g5jr-8080.app.github.dev/",
     headers: {
         'Content-Type': 'application/json'
     },
     withCredentials: true,
 });
-
-console.log('API Base URL:', process.env.REACT_APP_API_BASE_URL);
 
 export default api;
