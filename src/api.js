28545ca5e8e0cdf8cbd5f3de8b75e1d532ee607c
Rama Nagendra <ramanagendra2@gmail.com>
2024-06-27 13:10:31 +0530

    img add

--- src/api.js
+++ src/api.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: "https://symmetrical-carnival-wwp99rv5vvq2g5jr-8080.app.github.dev/",
+    
     headers: {
         'Content-Type': 'application/json'
     },
